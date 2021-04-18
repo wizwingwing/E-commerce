545aa1fd0c3a1c309f177e878f0b9ce8d3b7a3de
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-04-18 21:15:44 +0700

    Completed

--- src/Component/Flashsale.js
+++ src/Component/Flashsale.js
@@ -4,20 +4,20 @@ import flash from '../Image/flash.svg'
 export default function Flashsale() {
     return (
         <div className="FlashSale-Container">
-            <div className="flashsale-box-white">
-                <div className="flash-text">
-                <p>Flash</p>
-                <span>Sale</span>
-            </div>
-                <div className="icon-flash">
-                    <div className="icon-flash1">
-                        <img src={flash} alt=""/>
-                    </div>
-                    <div className="icon-flash2">
-                        <img src={flash} alt=""/>
+                <div className="flashsale-box-white">
+                    <div className="flash-text">
+                    <p>Flash</p>
+                </div>
+                    <div className="icon-flash">
+                        <div className="icon-flash1">
+                            <img src={flash} alt=""/>
+                        </div>
+                    <span>Sale</span>
+                        <div className="icon-flash2">
+                            <img src={flash} alt=""/>
+                        </div>
                     </div>
                 </div>
-            </div>
             <div className="flashsale-box-blue">
 
             </div>

d442cf3bc47392ce46c6709828c8b0e17c2b2ef8
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-04-18 19:24:22 +0700

    completed

--- src/Component/Flashsale.js
+++ src/Component/Flashsale.js
@@ -5,9 +5,10 @@ export default function Flashsale() {
     return (
         <div className="FlashSale-Container">
             <div className="flashsale-box-white">
-                <div className="flash-text"></div>flash-text
+                <div className="flash-text">
                 <p>Flash</p>
                 <span>Sale</span>
+            </div>
                 <div className="icon-flash">
                     <div className="icon-flash1">
                         <img src={flash} alt=""/>
